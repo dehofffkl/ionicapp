44b1f3e043cefaacef0c94451fef0ba0f0351219
dehofffkl <dehoffkl@dukes.jmu.edu>
2016-05-02 08:33:59 -0400

    Added files via upload

---
+++ www/js/controllers.js
@@ -0,0 +1,32 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  
+
+  
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+ .controller('ChatsCtrl', function($scope, Chats) {
+  
+
+  
+  $scope.friends = Friends.all();
+  $scope.remove = function(friend) {
+    Friends.remove(friend);
+  };
+})
+});
